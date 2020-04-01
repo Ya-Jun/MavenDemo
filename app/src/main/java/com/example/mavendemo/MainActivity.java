7271772e0a7316cd827c982c758b235e2d59782a
王亚军 <yajun.wang@kuwo.cn>
2020-04-01 18:38:57 +0800

    提交单独Lib的上传

--- app/src/main/java/com/example/mavendemo/MainActivity.java
+++ app/src/main/java/com/example/mavendemo/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.mavendemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mylibrary1.MyLibrary1;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textview);
+
+        textView.setText(MyLibrary1.getMessage1());
     }
 }
---
+++ mylibrary1/src/main/java/com/example/mylibrary1/MyLibrary1.java
@@ -0,0 +1,9 @@
+package com.example.mylibrary1;
+
+public class MyLibrary1 {
+
+    public static String getMessage1(){
+        return "我是Library1返回的";
+    }
+
+}
